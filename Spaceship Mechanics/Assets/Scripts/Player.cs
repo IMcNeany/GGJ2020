30485140e006127f740e4121832aaabbb3a5556f
Jacob Carr <jcarr343@hotmail.co.uk>
2020-01-31 22:31:46 +0000

    Basic movement

---
+++ Spaceship Mechanics/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            body.AddForce(Vector2.up * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(Vector2.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(Vector2.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(Vector2.right * speed * Time.deltaTime);
+        }
+    }
+}
