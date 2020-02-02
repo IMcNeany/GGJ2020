ccb0d9106162411c5b33d5c58fef6d8d9d8bfb9b
Isobel McNeany <icmc112@yahoo.co.uk>
2020-02-02 11:38:20 +0000

    Lose, win and start screen done and linked up

---
+++ Spaceship Mechanics/Assets/GameOverNavigation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverNavigation : MonoBehaviour
+{
+  //  SceneSwitcher switcher; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  switcher = gameObject.GetComponent<SceneSwitcher>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnReplayPressed()
+    {
+        // switcher.LoadScene()
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnMenuPressed()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
