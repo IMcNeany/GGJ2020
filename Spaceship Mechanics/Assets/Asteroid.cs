08f425173f14bd49d7a9ece52080ee6cfc1f4144
Unknown <Elliot2.martin@live.uwe.ac.uk>
2020-02-02 15:54:37 +0000

    uyuygiy7

---
+++ Spaceship Mechanics/Assets/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float timer = 180.0f;
+    private float current_timer = 0.0f;
+    public float power = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        current_timer += Time.deltaTime;
+        if(current_timer >= timer)
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * power);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Spaceship Mechanics/Assets/AudioFadeIn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFadeIn : MonoBehaviour
+{
+    public AudioSource audio;
+    private float max_volume = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        max_volume = 0.4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audio.volume < max_volume)
+        {
+
+
+            audio.volume += 0.05f * Time.deltaTime;
+        }
+        
+    }
+}
