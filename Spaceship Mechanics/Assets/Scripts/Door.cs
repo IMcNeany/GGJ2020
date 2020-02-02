43e97c3d21ded7bc1c26330a3a6f41348173daea
nataliaptzk <nataliaptzk@gmail.com>
2020-02-02 00:36:00 +0000

    Added Door Open/Close Animation

---
+++ Spaceship Mechanics/Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class Door : MonoBehaviour
+{
+    public bool isUnlocked;
+
+    private void Start()
+    {
+        UpdateLights();
+    }
+
+    public void UpdateLights()
+    {
+        if (isUnlocked)
+        {
+            transform.GetChild(0).GetChild(0).GetComponent<Light2D>().color = Color.green;
+            transform.GetChild(1).GetChild(0).GetComponent<Light2D>().color = Color.green;
+        }
+        else
+        {
+            transform.GetChild(0).GetChild(0).GetComponent<Light2D>().color = Color.red;
+            transform.GetChild(1).GetChild(0).GetComponent<Light2D>().color = Color.red;
+        }
+    }
+}
---
+++ Spaceship Mechanics/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.HasComponent<Door>())
+        {
+            if (other.GetComponent<Door>().isUnlocked)
+            {
+                var animator = other.GetComponent<Animator>();
+
+                animator.SetBool("IsPlayerStanding", true);
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.HasComponent<Door>())
+        {
+            if (other.GetComponent<Door>().isUnlocked)
+            {
+                var animator = other.GetComponent<Animator>();
+
+                animator.SetBool("IsPlayerStanding", false);
+            }
+
+        }
+    }
+}
