ea087997323559321b4ef2713574538610ac8f8c
Jacob Carr <jcarr343@hotmail.co.uk>
2020-02-02 14:23:42 +0000

    Applied fix to fuel as well

--- Spaceship Mechanics/Assets/FuelPickup.cs
+++ Spaceship Mechanics/Assets/FuelPickup.cs
@@ -20,7 +20,7 @@ public class FuelPickup : MonoBehaviour
 
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    public void OnCollisionEnter2D(Collision2D collision)
     {
 
         if (collision.gameObject.tag == ("Player"))
